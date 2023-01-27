9b101d0016af96a62fc461cf41e4907aa48a919f
FreddyJMo <122354621+FreddyJMo@users.noreply.github.com>
2023-01-27 04:26:39 -0700

    Add files via upload

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        //Here we will establish the reference of the script that we are going to use
+        Reference scriptureReference = new Reference("Proverbs", "3", "5-6");
+        
+        //We establish the verses of the scripture and pass the scripture Reference that was just created
+        Scripture scripture = new Scripture (scriptureReference, "Trust in the Lord with all thine heart; and lean not unto thine own understanding. In all thy ways acknowledge him, and he shall direct thy paths.");
+        
+        //We create the ScriptureMemorizer and pass the scripture we just created
+        ScriptureMemorizer scriptureMemorizer = new ScriptureMemorizer(scripture);
+
+        //We set the initial input to be empty and create a while loop
+        //That will terminate when the words run out or when the user types "quit"
+        string userInput = "";
+        while (userInput != "quit" && scriptureMemorizer.hasWordLeft() == true)
+        {
+            Console.Clear();
+            Console.WriteLine(string.Format("{0} {1}", scriptureReference.toString(), scriptureMemorizer.toString()));
+            Console.WriteLine();
+            Console.ReadLine();
+            scriptureMemorizer.removeWordsFromText();
+        }
+    
+    
     }
 }
