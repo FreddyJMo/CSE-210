0a96b9e4da9a263015fc3ddc5a2086885d165f09
FreddyJMo <122354621+FreddyJMo@users.noreply.github.com>
2023-02-09 02:31:31 -0700

    Develop4

---
+++ prove/ListingActivity.cs
@@ -0,0 +1,29 @@
+using System;
+
+public class ListingActivity : Activity
+{
+    public string _listingPrompt, _responsePrompt = "";
+
+    public void RunListing();
+    public void ProvidePrompt()
+    {
+        Random rnd = new Random();
+        List<string> prompts = new List<string>();
+        prompts.Add("Who are people that you appreciate?");
+        prompts.Add("What are personal strenghts of yours?");
+        prompts.Add("Who are people that you have helped this week?");
+        prompts.Add("When have you felt the Holy Ghost this month?");
+        prompts.Add("Who are some of your personal heroes?");
+
+        int index = rnd.Next(0,4);
+        Console.WriteLine(prompts[index]);
+        _listingPrompt = prompts[index];
+    }
+    public void EnterResponse()
+    {
+        _responsePrompt = Console.ReadLine();
+    }
+
+    public int ListAndCount();
+
+}
