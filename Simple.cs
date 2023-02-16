05a7b631ea00c13208fdc89574adc20642ad8338
FreddyJMo <122354621+FreddyJMo@users.noreply.github.com>
2023-02-16 02:19:32 -0700

    Delete Simple.cs

--- Simple.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-public class Simple : Goals
-{
-    public Simple(string type, string name, string description, int points) : base (type, name, description, points)
-    {
-        //Returns the values of the Goals base
-    }
-}
