da325cf5b4f741ffa06b5519dfb76b616718359c
FreddyJMo <122354621+FreddyJMo@users.noreply.github.com>
2023-02-16 02:18:43 -0700

    Delete Eternal.cs

--- Eternal.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-public class EternalGoal : Goals
-{
-    public EternalGoal(string type, string name, string description, int points) : base(type, name, description, points)
-    {
-        //Returns the values of the Goals base
-    }
-}
